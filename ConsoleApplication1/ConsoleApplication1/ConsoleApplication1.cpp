69010333fe9dba88a19f43f9f27af2b41cb80503
Jose Dias <zfc.dias@hotmail.com>
2015-11-17 18:18:18 +0000

    Pedido de dados de cliente

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,21 +8,60 @@ typedef struct ficha   // ficha de cliente
 {
 	char nome[45];  // Nome do cliente, char
 	int idade;      // Idade do Cliente
-	int id;         //Identificação do Cliente
-	int nr_res;     //número de reservas do cliente 0 > nr_res <6
+	int id;         // Identificação do Cliente
+	int nr_res;     // Número de reservas do cliente 0 > nr_res <6
 };
 
-typedef struct lugares
+typedef struct sala  // Lugares reservados/disponiveis
 {
-	char lugar[11][10];
+	char lugar[10];
 };
 
+
+ficha recolha_cliente()
+{
+	ficha c;
+	printf("Nome do Cliente:\n");
+	gets_s(c.nome);
+
+	printf("Idade do Cliente:\n");
+	scanf_s("%d", &c.idade);
+
+	printf("Identidade do Cliente:\n");
+	scanf_s("%d", &c.id);
+
+	printf("Numero de Reservas:\n");
+	scanf_s("%d", c.nr_res);
+
+	return c;
+}
+
+void imprime_ficha(ficha c)
+{
+	printf("Nome do Cliente: %s\n", c.nome);
+	printf("Idade do Cliente:\n", c.idade);
+	printf("Identidade do Cliente:\n", c.id);
+	printf("Numero de Reservas do Cliente:\n", c.nr_res);
+}
 
 int main()
 {
+	ficha cliente[2];
+	int i;
+	i = 0;
+	printf("Introduza os dados do cliente nr: %d\n", (i+1));
 	
+	for (i = 0; i < 2; i++)
+	{
+		cliente[i] = recolha_cliente();
+    }
 
-	printf("laaaaaa");
+	printf_s("Dados dos Clientes:\n");
+	for (i = 0; i < 2; i++)
+	{
+		imprime_ficha(cliente[i]);
+	}
+
     return 0;
 }
 
